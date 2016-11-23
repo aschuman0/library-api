846ed109d10ea333be8dc2a1019b109d9fcb5a8c
Aaron Schuman <aaron.schuman@urbanairship.com>
2016-11-22 16:07:22 -0800

    working isbn lookup endpoint

---
+++ library_api/index.js
@@ -0,0 +1,64 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+var morgan = require('morgan');
+var restful = require('node-restful');
+var isbn = require('node-isbn');
+var mongoose = restful.mongoose;
+
+var app = express();
+
+let apiBaseUrl = '/api/v1';
+ 
+app.use(morgan('dev'));
+
+app.use(bodyParser.urlencoded({'extended':'true'}));
+app.use(bodyParser.json());
+app.use(bodyParser.json({type:'application/vnd.api+json'}));
+
+app.use(methodOverride());
+
+// isbn lookup and return
+
+
+app.post(apiBaseUrl + '/books/isbn_lookup/', function(req, res) {
+    var isbnInput = req.body.isbn;
+
+    isbn.resolve(isbnInput, function(err, book) {
+        if (err) {
+            res.statusCode = 404;
+        } else {
+            res.send(book);
+        }
+    });
+});
+
+// persistant storage in mongodb
+
+mongoose.connect("mongodb://localhost/books");
+
+var bookSchema = mongoose.Schema({
+    title: String,
+    author: Array,
+    publisher: String,
+    description: String,
+    pages: Number,
+    language: String,
+    isbn: String,
+    thumb_url: String,
+    thumb_url_sm: String,
+    info_url: String,
+},{
+    timestamps: {
+        createdAt: 'created',
+        updatedAt: 'updated',
+    }
+});
+
+var Books = app.resource = restful.model('book', bookSchema)
+  .methods(['get', 'post', 'put', 'delete']);
+
+Books.register(app, apiBaseUrl + '/books');
+
+// need callback with info on running
+app.listen(3000);
