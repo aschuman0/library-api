2b46b67479fa7371938c81a5b0ea197658cac59e
Aaron Schuman <aaron@aschuman.net>
2016-12-04 13:48:05 -0800

    ok

--- library_api/index.js
+++ library_api/index.js
@@ -23,7 +23,7 @@ app.use(bodyParser.json({type:'application/vnd.api+json'}));
 app.use(methodOverride());
 
 // isbn lookup and return
-app.get(apiBaseUrl + '/books/isbn_lookup/', function(req, res) {
+app.post(apiBaseUrl + '/books/isbn_lookup/', function(req, res) {
     if(req.body.isbn) {
         isbn.resolve(req.body.isbn, function(err, book) {
             if (err) {
